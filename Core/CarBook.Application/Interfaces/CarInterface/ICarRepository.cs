ef0b03f14bc4fd97b4cb22efd41101f8406036a4
Orçun Aşık <asikorcun@gmail.com>
2023-11-25 18:00:28 +0300

    ICarRepository interface with GetCarsListWithBrand method has been added and implemented.

---
+++ Core/CarBook.Application/Interfaces/CarInterface/ICarRepository.cs
@@ -0,0 +1,8 @@
+﻿using CarBook.Domain.Entities;
+
+namespace CarBook.Application.Interfaces.CarInterface;
+
+public interface ICarRepository
+{
+    Task<IList<Car>> GetCarsListWithBrandAsync();
+}
---
+++ Infrastructure/CarBook.Persistence/Repositories/CarRepository/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarBook.Application.Interfaces.CarInterface;
+using CarBook.Domain.Entities;
+using CarBook.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBook.Persistence.Repositories.CarRepository;
+
+public class CarRepository : ICarRepository
+{
+    private readonly CarBookContext _context;
+
+    public CarRepository(CarBookContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IList<Car>> GetCarsListWithBrandAsync()
+    {
+        List<Car> datas = await _context.Cars.Include(c => c.Brand).ToListAsync();
+        return datas;
+    }
+}
